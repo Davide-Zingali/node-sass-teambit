0767368a10cfa4f4eca30e30f9111deeb3ed9e44
Davide <kalelc351k@gmail.com>
2020-12-18 16:29:05 +0100

    nav ed header base

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('src/app.js', 'dist/').sass('src/app.scss', 'dist/').copy('src/*.html', 'dist/');
+mix.js('src/app.js', 'dist/').sass('src/app.scss', 'dist/').copy('src/*.html', 'dist/').options({ processCssUrls: false });
 
 // Full API
 // mix.js(src, output);
